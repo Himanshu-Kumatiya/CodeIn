d1433b6886b2219328f8754cdb4d7170847bd798
Himanshu kumatiya <kumatiya123@gmail.com>
2024-11-09 12:04:14 +0530

    first commit

---
+++ client/src/components/common/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+            Developed by <a className="text-sky-400" href="https://github.com/Himanshu-Kumatiya">Himanshu Kumatiya</a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
